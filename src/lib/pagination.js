5ae1ba7913b6bc75ee86cda8088831136713315d
moon497 <moon497@edkim.co>
2018-06-07 17:46:12 +0900

    Chore: .

--- src/lib/pagination.js
+++ src/lib/pagination.js
@@ -1,16 +1,15 @@
 /**
  * 페이지네이션 객체
- * 
+ *
  * @author Moon SeongHwan <moon497&edkim.co>
  */
 class pagination {
   /**
-   * req를 받아서 페이지네이션 결과값으로 리턴해주는 함수
-   * 
+   * req.query를 받아서 페이지네이션 결과값으로 리턴해주는 함수
+   *
    * @memberof pagination
    */
-  static getPagination(page = 1, pageSize = 10) {
-
+  static getPagination({ page = 1, pageSize = 10 }) {
     const limit = Number(pageSize)
     const offset = (Number(page) - 1) * Number(pageSize)
 
